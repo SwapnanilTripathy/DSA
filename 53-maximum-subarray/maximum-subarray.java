f7ef17c053f53c1f35c0c4e6dd718054338f985a
Swamp <swapnaniltripathy2006@gmail.com>
2026-07-31 16:41:38 +0530

    Time: 1 ms (99.95%) | Memory: 80.5 MB (10.03%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxi = Integer.MIN_VALUE;
+        int maxi = nums[0];
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
