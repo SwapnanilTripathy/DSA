7fab46f9d5a5678f876f01e99e1d9a0637d99302
Swamp <swapnaniltripathy2006@gmail.com>
2026-07-31 07:37:19 +0530

    Time: 2 ms (70.85%) | Memory: 55.7 MB (46.72%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt = 0;
+        int el = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (nums[i] == el) cnt++;
+            else cnt--;
+        }
+        return el;
+    }
+}
