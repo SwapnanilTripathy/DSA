64982ed377c65b090fa744fe1cd776016721c058
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-04 07:47:00 +0530

    Time: 9 ms (36.42%) | Memory: 48.8 MB (92.72%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        // List<int[]> ans=new ArrayList<>();
+        // int n=intervals.length;
+        // for(int i=0;i<n;i++){
+        //     int start=intervals[i][0];
+        //     int end=intervals[i][1];
+        //     if(!ans.isEmpty()&&end<=ans.get(ans.size()-1)[1]) continue;
+        //     for(int j=i+1;j<n;j++){
+        //         if(end>=intervals[j][0]){
+        //             end=Math.max(end,intervals[j][1]);
+        //         }else{
+        //             break;
+        //         }
+        //     } 
+        //     ans.add(new int[]{start,end});
+        // }
+        // return ans.toArray(new int[ans.size()][]);
+        
+        List<int[]> ans=new ArrayList<>();
+        for(int i=0;i<intervals.length;i++){
+            int start=intervals[i][0];
+            int end=intervals[i][1];
+            if(ans.isEmpty() || start>ans.get(ans.size()-1)[1]){
+                ans.add(new int[]{start,end});
+            }
+            else{
+                ans.get(ans.size()-1)[1]=Math.max(end,ans.get(ans.size()-1)[1]);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
