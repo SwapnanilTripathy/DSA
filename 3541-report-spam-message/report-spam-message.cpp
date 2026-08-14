d901ba1707357038a1d479d74a644089bb16f928
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-14 22:19:27 +0530

    Time: 482 ms (11.01%) | Memory: 291 MB (33.33%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        int n = message.size();
+        int k = bannedWords.size();
+        set<string> banned;
+        for(int i = 0; i < k; i++) {
+            banned.insert(bannedWords[i]);
+        }
+        int ct = 0;
+        for(int i = 0; i < n; i++) {
+            if(banned.count(message[i])) ct++;
+            if(ct >= 2) return true;
+        }
+        return false;
+    }
+};
