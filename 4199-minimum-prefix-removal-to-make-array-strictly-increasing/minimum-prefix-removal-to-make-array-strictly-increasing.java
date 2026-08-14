ebf64d6db14bd6df7fa1bce6367a022de2ab387f
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-14 22:29:15 +0530

    Time: 1 ms (96.72%) | Memory: 120.3 MB (31.15%) - LeetSync

--- 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.java
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minimumPrefixLength(int[] nums) {
-        int index = 0;
-        int n = nums.length;
-        for(int i = 0; i < n - 1; i++) {
-            if(nums[i] >= nums[i + 1]) {
-                index = i+1;
-            }
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]>=nums[i+1]) return i+1;
         }
-        return index;
+        return 0;
     }
 }
