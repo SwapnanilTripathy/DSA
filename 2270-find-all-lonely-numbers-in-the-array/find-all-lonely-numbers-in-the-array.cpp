faad86d6bb3061c99fb18ad8831c3d80af443b47
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-16 22:29:33 +0530

    Time: 663 ms (5.16%) | Memory: 240.2 MB (5.23%) - LeetSync

--- 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
@@ -10,11 +10,9 @@ public:
 
         for (int i = 0; i < nums.size(); i++){
             if (mpp[nums[i]] == 1 && mpp[nums[i] - 1] == 0 && mpp[nums[i] + 1] == 0){
-                
                 ans.push_back(nums[i]);
             }
         }
-
         return ans;
     }
 };
