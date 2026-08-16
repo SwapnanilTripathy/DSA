ebe9d970838b1f6bb208123f8c3b8fa6a434879f
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-16 22:29:16 +0530

    Time: 721 ms (5.16%) | Memory: 240.3 MB (5.23%) - LeetSync

--- 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    vector<int> findLonely(vector<int>& nums) {
+    vector<int> findLonely(vector<int>& nums){
         vector<int> ans;
         map<int, int> mpp;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++){
             mpp[nums[i]]++;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (mpp[nums[i]] == 1 && mpp[nums[i] - 1] == 0 && mpp[nums[i] + 1] == 0) {
+        for (int i = 0; i < nums.size(); i++){
+            if (mpp[nums[i]] == 1 && mpp[nums[i] - 1] == 0 && mpp[nums[i] + 1] == 0){
                 
                 ans.push_back(nums[i]);
             }
