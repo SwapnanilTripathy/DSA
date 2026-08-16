727a8d5f684a92e7d08f3c0b7ca795c776622433
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-16 20:10:10 +0530

    Time: 9 ms (28.94%) | Memory: 13.1 MB (90.52%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,13 +5,11 @@ public:
         int ct = 0;
         int maxi = 0;
         for (int i = 0; i < n; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-                s[i] == 'o' || s[i] == 'u') {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
                 ct++;
             }
             if (i >= k) {
-                if (s[i-k] == 'a' || s[i-k] == 'e' || s[i-k] == 'i' ||
-                    s[i-k] == 'o' || s[i-k] == 'u') {
+                if (s[i-k] == 'a' || s[i-k] == 'e' || s[i-k] == 'i' || s[i-k] == 'o' || s[i-k] == 'u') {
                     ct--;
                 }
             }
