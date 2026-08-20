f553cb4058a8793c8e2fdc7ae993cd1bdd35bf3e
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-20 20:50:33 +0530

    Time: 6 ms (38.08%) | Memory: 15 MB (34.11%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int n=s.size();
+        int cta=0;
+        int ctb=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a') cta++;
+            else ctb++;
+        }
+    return (n-(2*min(cta,ctb)));   
+    }
+};
