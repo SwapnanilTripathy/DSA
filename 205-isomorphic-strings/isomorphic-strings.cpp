8a2989c68361ef922e37c712c1a95bab0e3cdca6
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-22 20:36:07 +0530

    Time: 4 ms (23.17%) | Memory: 9.2 MB (83.28%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s,string t) {
+        int n=s.size();
+        if(n!=t.size()) return false;
+        int mpp1[256]={0};
+        int mpp2[256]={0};
+        for(int i=0;i<n;i++){
+            if(mpp1[s[i]]!=mpp2[t[i]]) return false;
+            mpp1[s[i]]=i+1;
+            mpp2[t[i]]=i+1;
+        }
+        return true;
+    }
+};
