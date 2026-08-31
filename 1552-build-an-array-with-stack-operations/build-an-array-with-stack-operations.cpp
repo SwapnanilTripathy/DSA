b49ef8f6b4761eab6de38d769b2b69809e8432ba
Swamp <swapnaniltripathy2006@gmail.com>
2026-08-31 14:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.05%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int x=1;
+
+        for(int i=0;i<target.size();i++){
+            while(x<target[i]){
+                ans.push_back("Push");
+                ans.push_back("Pop");
+                x++;
+            }
+            ans.push_back("Push");
+            x++;
+        }
+
+        return ans;
+    }
+};
