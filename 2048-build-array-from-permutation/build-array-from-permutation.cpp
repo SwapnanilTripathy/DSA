1768e047e32b7696288fbdbc922a526db11098ce
Swamp <swapnaniltripathy2006@gmail.com>
2026-09-01 08:25:09 +0530

    Time: 3 ms (11.30%) | Memory: 20.8 MB (32.70%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -4,8 +4,7 @@ public:
         vector<int>ans;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            int value=nums[nums[i]];
-            ans.push_back(value);
+            ans.push_back(nums[nums[i]]);
         }
     return ans;    
     }
