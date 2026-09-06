17d9e066b1b730394e9cc9a733b5d5933cf99d2b
Swamp <swapnaniltripathy2006@gmail.com>
2026-09-06 11:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (59.50%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        ans.push_back({});
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            int sz=ans.size();
+            for(int j=0;j<sz;j++){
+                vector<int> temp=ans[j];
+                temp.push_back(nums[i]);
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
